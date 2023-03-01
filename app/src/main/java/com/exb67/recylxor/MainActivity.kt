66b2941e77bc126aa90604ef1d43ee4066022bc8
Samuel Bourassa <district67@gmail.com>
2023-03-01 11:12:03 -0500

    First commit.

---
+++ app/src/main/java/com/exb67/recylxor/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.exb67.recylxor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
