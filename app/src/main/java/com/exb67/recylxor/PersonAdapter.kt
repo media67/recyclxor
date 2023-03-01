1fc3064d20c625c18f97a4f0aecf799dbcf9af01
Samuel Bourassa <district67@gmail.com>
2023-03-01 12:24:57 -0500

    Create item_view.

--- app/src/main/java/com/exb67/recylxor/PersonAdapter.kt
+++ app/src/main/java/com/exb67/recylxor/PersonAdapter.kt
@@ -1,8 +1,24 @@
 package com.exb67.recylxor
 
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class PersonAdapter {
+class PersonAdapter(
+    var persons: List<Person>
+): RecyclerView.Adapter<PersonAdapter.PersonViewHolder>() {
 inner class PersonViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PersonViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        return persons.size
+    }
+
+    override fun onBindViewHolder(holder: PersonViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
 }
